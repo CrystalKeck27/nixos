7a45ddb54467b91b22437f925664570b4959ae69
Crystal Keck <meaneea@gmail.com>
2024-03-30 20:48:55 +0000

    Fix steam

--- hosts/galley/configuration.nix
+++ hosts/galley/configuration.nix
@@ -113,6 +113,12 @@
     wlr.enable = true;
   };
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   hardware = {
     opengl.enable = true;
   };
