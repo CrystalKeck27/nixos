5b4d4ca31fb0e0fe80bd5ec4540b19cd7271fb93
CrystalKeck27 <meaneea@gmail.com>
2024-06-05 22:16:40 +0000

    Added surrealdb to wsl. Cleaned up home/nixos/apps

--- home/nixos/apps/default.nix
+++ home/nixos/apps/default.nix
@@ -1,32 +1,13 @@
 {pkgs, ...}: {
   imports = [
-    ./discord
-    ./firefox
-    ./vscode
   ];
 
   home.packages = with pkgs; [
 
     # School
-    teams-for-linux
     rustup
-    # probe-rs
+    surrealdb
 
-    dunst
-    krita
-    obsidian
-    xfce.thunar
-
-    # Screenshot tools
-    grim
-    ffmpeg_6
-    slurp
-    vlc
-    wl-clipboard
-
-    # System
-    polkit_gnome
-    rofi-wayland
     tree
     unzip
   ];
