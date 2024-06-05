b144c242195ed294f6a093c72110e282ffecbcfb
Crystal Keck <Crystal.Keck@millerwelds.com>
2024-06-05 17:03:01 -0500

    fix flake.nix

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     waveforms.url = "github:CrystalKeck27/waveforms-flake";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-    inputs.vscode-server.url = "github:nix-community/nixos-vscode-server";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
   };
 
   outputs = { self, nixpkgs, nixos-wsl, vscode-server, home-manager, hyprland, waveforms, ... }@inputs:
